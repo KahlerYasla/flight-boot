e6966afa93ff77b58da7a1bf0c59eb4b6c3281fe
KahlerYasla <garpayyasla@gmail.com>
2024-01-27 21:50:03 +0300

    some trials for auth 😭

--- src/main/java/com/vafaill/flightboot/service/scheduled/FlightScheduledService.java
+++ src/main/java/com/vafaill/flightboot/service/scheduled/FlightScheduledService.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -23,16 +24,32 @@ public class FlightScheduledService {
     // Run every 10 seconds
     @Scheduled(fixedRate = 10000)
     public void fetchAndSaveFlightData() throws URISyntaxException, IOException, InterruptedException {
+        // Prepare request parameters
+        Map<String, String> params = Map.of(
+                "to", "LHE",
+                "from", "DXB",
+                "depart-date", "2015-03-31",
+                "return-date", "2015-04-07");
 
-        HttpClient httpClient = HttpClient.newHttpClient();
+        // Build URI with query parameters
+        URI uri = buildUriWithParams("https://test.api.amadeus.com/v1/shopping/flight-destinations", params);
 
+        // Build the HttpRequest
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://jsonplaceholder.typicode.com/posts/1"))
+                .uri(uri)
+                .header("", "e287c0e5ddmsha0042f63c38de1dp1d3c24jsn4cbc5c75dc9d")
+                .header("X-RapidAPI-Host", "siddiq-such-flight-v1.p.rapidapi.com")
                 .GET()
                 .build();
 
+        // Create HttpClient
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        // Send the request and get the response
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
+        // Print the response body
+        System.out.println("Response Headers: " + response.headers());
         System.out.println("Response Code: " + response.statusCode());
         System.out.println("Response Body: " + response.body());
 
@@ -41,5 +58,15 @@ public class FlightScheduledService {
         // TODO: Map JSON to DTO
 
         flightService.fetchFlightDataFromThirdPartyAPI();
+    }
+
+    private static URI buildUriWithParams(String baseUrl, Map<String, String> params) throws URISyntaxException {
+        StringBuilder uriBuilder = new StringBuilder(baseUrl);
+        uriBuilder.append("?");
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            uriBuilder.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+        }
+        uriBuilder.deleteCharAt(uriBuilder.length() - 1); // Remove the trailing "&"
+        return new URI(uriBuilder.toString());
     }
 }
